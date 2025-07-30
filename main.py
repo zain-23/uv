558da26c7b5486233c3ad74ea845bd397cab3612
zain-23 <zaingit780@gmail.com>
2025-07-30 13:32:46 +0500

    Refactor main.py to remove UVicorn dependency and add user product handling endpoint

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
-import uvicorn
+from schema.user import User, SignUp, Product
 
 app =  FastAPI(
     title="FastAPI",
 )
 
-@app.get("/")
-def read_root():
+@app.post("/user")
+def read_root(product: Product):
+    print(product)
     return {"message": "Hello World"}
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
